c2ec1993489dd669bd84f9946041f838f8b26076
James Ho <jamesho19@gmail.com>
2025-01-11 23:19:24 +1100

    Update error_handlers.py
    correct

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -31,9 +31,9 @@ def bad_request(error):
     message = str(error)
     app.logger.warning(message)
     return (
-        jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
-        ),
+    jsonify(
+        status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
+    ),
         status.HTTP_400_BAD_REQUEST,
     )
 
@@ -43,10 +43,9 @@ def not_found(error):
     """Handles resources not found with 404_NOT_FOUND"""
     message = str(error)
     app.logger.warning(message)
-    return (
+return (
     jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
-    status.HTTP_404_NOT_FOUND,
-    )
+status.HTTP_404_NOT_FOUND,)
 
 
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
